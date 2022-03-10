5cccd396534ee783877c938d2a0b6423a2ec355d
abdularshad06 <abdularshad390@gmail.com>
2022-03-10 12:24:18 +0530

    Update bkp_commands.sh

--- ldap-server/bkp_commands.sh
+++ ldap-server/bkp_commands.sh
@@ -1,4 +1,10 @@
+cat /etc/scripts/bkp_commands.sh
+
+
 #!/bin/bash
 
-slapcat -n 0 -l config.ldif
-slapcat -n 1 -l data.ldif
+slapcat -n 0 -l /home/backup/config.ldif
+slapcat -n 1 -l /home/backup/data.ldif
+
+# Print end status message.
+echo "Backup finished"
